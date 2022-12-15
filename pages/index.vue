27956be84b4984c957eb5c770eac27614753d738
aripadrian <aripadrian01@gmail.com>
2022-12-15 10:22:21 +0700

    mapbox init

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      map: {},
+    }
+  },
+  mounted() {
+    const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js')
+
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiYXJpcGFkcmlhbiIsImEiOiJja2xoYnF1a24xNmtiMm9xcHdsODAzbmZuIn0.rsPD86w82iXUMRZistT3Ww'
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 11,
+      center: [65, 95],
+    })
+  },
+}
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 100vh;
+}
+</style>
